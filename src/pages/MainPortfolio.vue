a9ad75264a6e50a0961ed02e42a2cfde89e36798
Rionn Kenshin Pineda <rionnken@outlook.com>
2025-11-01 03:41:23 +0800

    feat: add transitions for display card

--- src/pages/MainPortfolio.vue
+++ src/pages/MainPortfolio.vue
@@ -26,54 +26,82 @@ let openDisplay = (id) => {
     />
 
     <!-- Background -->
-    <div
-      v-if="displayOpen"
-      class="fixed bg-stone-800/50 w-screen h-screen top-0 left-0"
-      @click="displayOpen = false"
-    />
-    <div
-      v-if="displayOpen"
-      class="fixed top-1/2 left-1/2 -translate-1/2 bg-stone-800 w-1/2 rounded-md p-4 text-white"
-    >
-      <img
-        :src="currentDisplay.image"
-        alt=""
-        class="mb-4 rounded-md"
+    <Transition name="fade">
+      <div
+        v-if="displayOpen"
+        class="fixed bg-stone-800/50 w-screen h-screen top-0 left-0"
+        @click="displayOpen = false"
+      />
+    </Transition>
+    <!-- Dialog -->
+    <Transition name="fade">
+      <div
+        v-if="displayOpen"
+        class="fixed 
+          top-1/2
+          left-1/2 
+          -translate-1/2
+          bg-stone-800
+          w-1/2 
+          rounded-md 
+          p-4 
+          text-white
+        "
+        :class="displayOpen ? 'opacity-100' : 'opacity-0'"
       >
-      <h2 class="text-2xl font-bold mb-2 hover:underline">
-        {{ currentDisplay.title }}
-      </h2>
-      <p
-        v-for="(line, index) in currentDisplay.description"
-        :key="index"
-        class="mb-2"
-      >
-        {{ line }}
-      </p>
-      <a
-        :href="currentDisplay.url"
-        target="_blank"
-      >
-        <button 
-          class="
-            rounded-md 
-            bg-stone-300 
-            py-2 
-            px-4 
-            text-stone-800 
-            hover:bg-stone-400 
-            hover:cursor-pointer 
-            font-bold 
-            border-2 
-            border-stone-700
-          "
+        <img
+          :src="currentDisplay.image"
+          alt=""
+          class="mb-4 rounded-md mx-auto"
         >
-          Open Site
-        </button>
-      </a>
-    </div>
+        <h2 class="text-2xl font-bold mb-2 hover:underline">
+          {{ currentDisplay.title }}
+        </h2>
+        <p
+          v-for="(line, index) in currentDisplay.description"
+          :key="index"
+          class="mb-2"
+        >
+          {{ line }}
+        </p>
+        <a
+          v-if="currentDisplay.url"
+          :href="currentDisplay.url"
+          target="_blank"
+        >
+          <button 
+            class="
+              rounded-md 
+              bg-stone-300 
+              py-2 
+              px-4 
+              text-stone-800 
+              hover:bg-stone-400 
+              hover:cursor-pointer 
+              font-bold 
+              border-2 
+              border-stone-700
+            "
+          >
+            Open Site
+          </button>
+        </a>
+      </div>
+    </Transition>
   </div>
 </template>
 
 <script>
 </script>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
