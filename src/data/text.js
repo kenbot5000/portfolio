a5b18312cd0a52596a0bfe86278ce5e6c48de445
Rionn Kenshin Pineda <rionnken@outlook.com>
2025-11-01 03:50:13 +0800

    feat: add draft descriptions for display cards

--- src/data/text.js
+++ src/data/text.js
@@ -5,8 +5,27 @@ export const text = [
     description: [
       'A medical website from ByWinona, providing pharmaceutical services for women.',
       "Built with Vue.js and Tailwind CSS, built for responsiveness. This is by far the biggest project I've worked on to date, involving multiple team members including designers, backend developers, and QA testers. Spans multiple subprojects for various aspects of the service, including member portals and employee dashboards.",
+      'Most of what I worked on were the patient, doctor, and customer support dashboards, all utilizing Vue.js. Features of note were the patient onboarding form, prescription management, as well as chats between patients, doctors, and customer support representatives.',
     ],
     url: 'https://bywinona.com',
     image: 'https://placehold.co/800x400',
+  },
+  {
+    id: 'infiniscan',
+    title: 'Infiniscan',
+    description: [
+      "A web application as a proof of concept for a milktea shop's point of sale system.",
+      'Built with Vue.js+Vuetify, with an Express.js and MongoDB backend. Primarily a solo project, this was done for my thesis in college.',
+      'Features include a dynamic menu system, order management, and sales reporting. Also includes a QR scanner for order pickup verification.',
+    ],
+    image: 'https://placehold.co/800x400',
+  },
+  {
+    id: 'greenit',
+    title: 'Greenit',
+    description: [
+      'A simple mockup website for a fictional Reddit clone as a demo of my frontend skills.',
+      'Built with Vue and Vuetify.',
+    ],
   },
 ]
