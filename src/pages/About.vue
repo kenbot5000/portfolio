9568c76dac4716f2f4017574af88f0815b9c0c74
Rionn Kenshin Pineda <rionnken@outlook.com>
2025-11-02 20:09:16 +0800

    feat: add basic about page placeholder

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,3 +1,24 @@
 <template>
-  <div>about</div>
+  <div
+    class="
+      mx-20
+      mt-10
+      px-4
+      text-white
+      bg-[#21221D]
+      rounded-md 
+      *:my-4
+      *:text-lg
+    "
+  >
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae bibendum velit. Nam elementum sollicitudin turpis id auctor. Cras convallis convallis sem, id tempor lacus posuere eu. Integer eget nulla sollicitudin, tristique purus ac, rhoncus eros. Morbi vel diam non lacus finibus elementum. In pulvinar cursus mauris, in ultrices enim lacinia in. Curabitur posuere rutrum mi, et aliquam sapien tempor at. Proin et justo luctus, eleifend mi eget, consequat nisi. Aenean est augue, tempor sit amet est vel, vulputate sollicitudin ante. Suspendisse consectetur in mauris et rutrum.
+    </p>
+    <p>
+      Nullam eget urna mauris. Curabitur pharetra mauris eget mi commodo, ut commodo orci bibendum. In hac habitasse platea dictumst. Nam scelerisque eget sapien rutrum hendrerit. Mauris sollicitudin, felis nec maximus placerat, enim ex hendrerit magna, nec feugiat enim ante sed sem. Curabitur tincidunt, justo quis lobortis dignissim, diam dolor vulputate ligula, pulvinar sagittis mi nisi quis metus. Sed euismod malesuada orci, sed viverra mi vestibulum id. Sed ullamcorper lacus tincidunt est fermentum euismod.
+    </p>
+    <p>
+      Maecenas aliquam condimentum vulputate. Proin accumsan lectus dignissim, egestas tortor in, feugiat ligula. Cras consequat urna quis augue volutpat, ac mollis sem sollicitudin. Donec eget facilisis nibh. Nunc interdum orci ut pellentesque placerat. Integer vehicula elit eget augue iaculis, sed rhoncus est vehicula. Fusce a tincidunt augue, sed euismod tortor. Cras ac justo tincidunt urna laoreet imperdiet eget et dui.
+    </p>
+  </div>
 </template>
