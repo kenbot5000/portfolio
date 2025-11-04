b4c849aea61cc067696a4f2e7f712c480c287e1e
Rionn Kenshin Pineda <rionnken@outlook.com>
2025-11-05 04:16:22 +0800

    feat: add draft about message

--- src/pages/AboutMe.vue
+++ src/pages/AboutMe.vue
@@ -12,13 +12,13 @@
     "
   >
     <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae bibendum velit. Nam elementum sollicitudin turpis id auctor. Cras convallis convallis sem, id tempor lacus posuere eu. Integer eget nulla sollicitudin, tristique purus ac, rhoncus eros. Morbi vel diam non lacus finibus elementum. In pulvinar cursus mauris, in ultrices enim lacinia in. Curabitur posuere rutrum mi, et aliquam sapien tempor at. Proin et justo luctus, eleifend mi eget, consequat nisi. Aenean est augue, tempor sit amet est vel, vulputate sollicitudin ante. Suspendisse consectetur in mauris et rutrum.
+      I'm an experienced fullstack web dev with over 5 years of development experience under my belt. I primarily work with the MEVN stack (though AWS is mostly used in my current project), but most experienced in front-end development using Vue. Most of my work is done as a developer for Winona, a medical company where I developed several projects from scratch, which are used by the various members of the company.
     </p>
     <p>
-      Nullam eget urna mauris. Curabitur pharetra mauris eget mi commodo, ut commodo orci bibendum. In hac habitasse platea dictumst. Nam scelerisque eget sapien rutrum hendrerit. Mauris sollicitudin, felis nec maximus placerat, enim ex hendrerit magna, nec feugiat enim ante sed sem. Curabitur tincidunt, justo quis lobortis dignissim, diam dolor vulputate ligula, pulvinar sagittis mi nisi quis metus. Sed euismod malesuada orci, sed viverra mi vestibulum id. Sed ullamcorper lacus tincidunt est fermentum euismod.
+      As for other tools that I use for creating projects: I've used Express.js for backend work, and have used Figma to create designs. I use Python, as well as different game engines to make small games and tools. I've also created small Discord chat bots using Python in the past. I like to dabble in development skills beyond web, and I'm always willing to learn something new to pull off a project.
     </p>
     <p>
-      Maecenas aliquam condimentum vulputate. Proin accumsan lectus dignissim, egestas tortor in, feugiat ligula. Cras consequat urna quis augue volutpat, ac mollis sem sollicitudin. Donec eget facilisis nibh. Nunc interdum orci ut pellentesque placerat. Integer vehicula elit eget augue iaculis, sed rhoncus est vehicula. Fusce a tincidunt augue, sed euismod tortor. Cras ac justo tincidunt urna laoreet imperdiet eget et dui.
+      In my free time, I like to play video games and cosplay :) I also own a cat and a dog! 
     </p>
   </div>
 </template>
