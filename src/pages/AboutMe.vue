f19532df26cd9e996bde3f3c2ddad866f641005d
Rionn Kenshin Pineda <rionnken@outlook.com>
2025-11-06 05:51:26 +0800

    fix: remove extra margin on about

--- src/pages/AboutMe.vue
+++ src/pages/AboutMe.vue
@@ -2,11 +2,10 @@
   <div
     class="
       mx-20
-      mt-10
       px-4
       text-white
       bg-[#21221D]
-      rounded-md 
+      rounded-md
       *:my-4
       *:text-lg
     "
@@ -18,7 +17,7 @@
       As for other tools that I use for creating projects: I've used Express.js for backend work, and have used Figma to create designs. I use Python, as well as different game engines to make small games and tools. I've also created small Discord chat bots using Python in the past. I like to dabble in development skills beyond web, and I'm always willing to learn something new to pull off a project.
     </p>
     <p>
-      In my free time, I like to play video games and cosplay :) I also own a cat and a dog! 
+      In my free time, I like to play video games and cosplay :) I also own a cat and a dog!
     </p>
   </div>
 </template>
